91115f44fe1957358a25e295afd9ec808d025554
たことぱす <79318163+tacotopasu@users.noreply.github.com>
2023-09-17 18:18:11 +0100

    Added Toggle Sprint
    -Added Echo, will replace using Chatlib.chat in most cases
    - Initiated settings.js

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
-register("command", () => {
-    ChatLib.chat("Hello World!");
+// Variables
+var tag = "&3[&bSharkie&3] &r"
+
+// Unobfuscated functions: https://github.com/Deadrik/TFC2/blob/master/custommappings/methods.csv
+// Unobfuscated fields: https://github.com/KevyPorter/Minecraft-Forge-Utils/blob/master/fields.csv
+
+// Modules State
+var toggleSprintOn = false;
+
+// Client Functions
+
+function Echo(text){
+    ChatLib.chat(`${tag}${text}`);
+}
+
+function ToggleModule(){
+
+} // TODO
+
+
+register("command", () => { // Hello World
+    Echo("Hello World!");
 }).setName("hello");
+
+register("command", () => { // Niko ❤
+    ChatLib.chat(ChatLib.getCenteredText("&d❤&r I &dlove &ryou niko! &d❤"));
+}).setName("niko");
+
+
+register('step', toggleSprintStep); // Toggle Sprint Step Function
+var sprintOn = false;
+function toggleSprintStep() {
+    if (!toggleSprintOn) return;
+    if (Player.getMotionX() == 0 && Player.getMotionZ() == 0) return;
+    var keyBind = new KeyBind(Client.getMinecraft().field_71474_y.field_151444_V); // gameSettings.keyBindSprint
+    // if (keyBind.isPressed() && !sprintOn) sprintOn = true;
+    // if (keyBind.isPressed() && sprintOn) sprintOn = false;
+    if (keyBind.isPressed()) sprintOn = !sprintOn;
+    keyBind.setState(sprintOn);
+}
+
+register("renderOverlay", () => { // Toggle Sprint Indicator (HUD)
+    if(!sprintOn || !toggleSprintOn) return;
+	
+	var width = Renderer.screen.getWidth();
+	var height = Renderer.screen.getHeight();
+	
+	Renderer.drawString("&3[&bSprinting Toggled&3]", 5, height - Client.getMinecraft().field_71466_p.field_78288_b - 5); // fontRenderer.FONT_HEIGHT    
+});
+
+register('command', function() { // Toggle Sprint Module Command
+    if (toggleSprintOn) {
+        toggleSprintOn = false;
+        Echo("&lToggle Sprint&r Disabled.");
+    } else {
+        toggleSprintOn = true;
+        Echo("&lToggle Sprint&r Enabled.");
+    }
+}).setCommandName('togglesprint');
+
---
+++ settings.js
@@ -0,0 +1,5 @@
+class Settings {
+
+    // We'll most likely use a class to construct the Settings, but I don't know how that works yet x3
+
+}
